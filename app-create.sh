5754c7eb1e64cbb264a714b160e4becb5a990500
Niklaus Hirt <niklaushirt@gmail.com>
2020-02-10 11:06:54 +0100

    new

---
+++ app-create.sh
@@ -0,0 +1,5 @@
+kubectl apply -f namespace.yaml
+kubectl apply -f 1-app.yaml
+kubectl apply -f 2-app-channel.yaml
+kubectl apply -f 3-app-web.yaml
+
---
+++ app-delete.sh
@@ -0,0 +1,4 @@
+#kubectl delete -f namespace.yaml
+kubectl delete -f 1-app.yaml
+kubectl delete -f 2-app-channel.yaml
+kubectl delete -f 3-app-web.yaml
