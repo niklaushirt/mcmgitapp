5f7b01c559604f01531a3c569500703782b8b2a0
Niklaus Hirt <niklaushirt@gmail.com>
2020-03-12 16:40:47 +0100

    Separate Components

--- app-create.sh
+++
@@ -1,5 +0,0 @@
-kubectl apply -f namespace.yaml
-kubectl apply -f 1-app.yaml
-kubectl apply -f 2-app-channel.yaml
-kubectl apply -f 3-app-web.yaml
-
--- app-delete.sh
+++
@@ -1,4 +0,0 @@
-#kubectl delete -f namespace.yaml
-kubectl delete -f 1-app.yaml
-kubectl delete -f 2-app-channel.yaml
-kubectl delete -f 3-app-web.yaml
